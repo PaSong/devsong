fbcd543c3f21a89f1516c0090207294ad485abb8
PaSong <hwain25@naver.com>
2018-11-30 01:08:25 +0900

    juno practice

---
+++ index_skin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>junotest</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+  <script src="main.js"></script>
+  <link rel="stylesheet" href="./css/index.css">
+</head>
+<body>
+  <div class="wrap">
+    <div class="nav">
+      <div class="contain">
+        <div class="log"></div>
+        <div class="nav_list">
+          <ul>
+            <li><a href="#">회사소개</a></li>
+            <li><a href="#">제품소개</a></li>
+            <li><a href="#">시공사례</a></li>
+            <li><a href="#">고객지원</a></li>
+            <li><a href="#">문의하기</a></li>
+            <li><a href="#">예약사이트바로가기</a></li>
+          </ul>
+        </div>
+      </div>
+    </div><!--//nav-->
+  </div><!--//wrap -->
+</body>
+</html>
